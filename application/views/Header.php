eed66afaa65dc46e6e8be26718b88237e70dc6a7
vvrvishal <vishalguptaramasare@gmail.com>
2023-01-07 01:30:21 +0530

    changes by vishal

--- application/views/Header.php
+++ application/views/Header.php
@@ -35,7 +35,9 @@
 			integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
 			crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/izitoast/1.4.0/css/iziToast.min.css" integrity="sha512-O03ntXoVqaGUTAeAmvQ2YSzkCvclZEcPQu1eqloPaHfJ5RuNGiS4l+3duaidD801P50J28EHyonCV06CUlTSag==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/izitoast/1.4.0/css/iziToast.min.css"
+		  integrity="sha512-O03ntXoVqaGUTAeAmvQ2YSzkCvclZEcPQu1eqloPaHfJ5RuNGiS4l+3duaidD801P50J28EHyonCV06CUlTSag=="
+		  crossorigin="anonymous" referrerpolicy="no-referrer"/>
 
 
 	<title></title>
@@ -49,13 +51,14 @@
 </head>
 <body>
 <div class="container">
-	<div class="PoppinsFont align-items-center border-dark row mx-0 mx-md-3 pt-2 HeaderBorder" style="border-bottom: 2px solid;">
+	<div class="PoppinsFont align-items-center border-dark row mx-0 mx-md-3 pt-2 HeaderBorder"
+		 style="border-bottom: 2px solid;">
 		<div class="col-md-3 col-6">
 			<h1 class="BebasFont text-capitalize mb-0"> CARVERSAL</h1>
 		</div>
 		<div class="col-6 text-right d-block d-md-none">
 
-				<img src="<?= base_url() ?>/assets/CarvesalImage/User.jpg" class="mr-2 rounded-circle" alt="" style="
+			<img src="<?= base_url() ?>/assets/CarvesalImage/User.jpg" class="mr-2 rounded-circle" alt="" style="
 						width: 36px;">
 
 		</div>
@@ -63,15 +66,32 @@
 		<div class="col-md-6" id="CarversalMobileMenu">
 			<ul class="align-items-center d-flex justify-content-around list-unstyled mb-0" id="CarversalMenu">
 				<!--menu-->
-				<li class="MenuItems badge-pill px-0 mx-md-4 py-md-1 mt-3 "><a  href="<?=base_url()?>" class="<?php echo $this->uri->segment(1) == '' ? 'ActiveMenu' : '' ?>">Trending</a></li>
-				<li class="MenuItems badge-pill px-0 mx-md-4 py-md-1 mt-3 "><a  class="<?php echo  $this->uri->segment(1) == 'Discover' ? 'ActiveMenu' : '' ?>" href="<?=base_url()?>Discover">Discover</a></li>
-				<li class="MenuItems badge-pill px-0 mx-md-4 py-md-1 mt-3 "><a class="<?php echo $this->uri->segment(1) == 'Latest' ? 'ActiveMenu' : '' ?>" href="<?=base_url()?>Latest">Latest</a></li>
-				<li class="MenuItems badge-pill px-0 mx-md-4 py-md-1 mt-3 "><a  class="<?php echo $this->uri->segment(1) == 'Popular' ? 'ActiveMenu' : '' ?>" href="<?=base_url()?>Popular">Popular</a></li>
+				<li class="MenuItems badge-pill px-0 mx-md-2 py-md-1 mt-3 "><a href="<?= base_url() ?>"
+																			   class="<?php echo $this->uri->segment(1) == '' ? 'ActiveMenu' : '' ?>">Trending</a>
+				</li>
+				<li class="MenuItems badge-pill px-0 mx-md-2 py-md-1 mt-3 "><a
+							class="<?php echo $this->uri->segment(1) == 'Discover' ? 'ActiveMenu' : '' ?>"
+							href="<?= base_url() ?>Discover">Discover</a></li>
+				<li class="MenuItems badge-pill px-0 mx-md-2 py-md-1 mt-3 "><a
+							class="<?php echo $this->uri->segment(1) == 'Latest' ? 'ActiveMenu' : '' ?>"
+							href="<?= base_url() ?>Latest">Latest</a></li>
+				<li class="MenuItems badge-pill px-0 mx-md-2 py-md-1 mt-3 "><a
+							class="<?php echo $this->uri->segment(1) == 'Popular' ? 'ActiveMenu' : '' ?>"
+							href="<?= base_url() ?>Popular">Popular</a></li>
+				<li class="d-none d-md-block">
+					<div>
+						<input type="checkbox" class="checkbox" id="checkbox">
+						<label for="checkbox" class="label  ml-1">
+							<i class="fa-moon fas small"></i><i class="fa-sun fas small"></i>
+							<div class="ball"></div>
+						</label>
+					</div>
+				</li>
 			</ul>
 		</div>
 		<div class="col-md-3 d-md-block" style="display: none" id="SearchHere">
 			<div class="d-flex align-items-center mt-3 mt-md-0">
-				<i class="fa-left-long fa-solid mr-2" id="HideSearch" style="font-size: x-large;"></i>
+				<i class="fa-left-long fa-solid mr-2 d-block d-md-none" id="HideSearch" style="font-size: x-large;"></i>
 				<div class="SearchBar align-items-center align-items-sm-center w-100 badge-pill border border-dark d-f d-flex py-1">
 					<span><i class="fa-solid fa-magnifying-glass"></i></span>
 					<input type="search" class="border-0 form-control py-0"
@@ -81,45 +101,51 @@
 		</div>
 	</div>
 </div>
-<div class="container  MobileIconMenu" >
+</div>
+<div class="container  MobileIconMenu d-block d-md-none">
 	<div class="PoppinsFont align-items-center border-dark row mx-0 mx-md-3 py-2 HeaderBorder">
 		<div class="col-md-6">
 			<ul class="align-items-center d-flex justify-content-around list-unstyled mb-0" id="CarversalMenu">
 				<!--menu-->
-				<li class="MenuItems  px-0 mx-md-4 py-md-1 mt-3 "><a  href="<?=base_url()?>" class="<?php echo $this->uri->segment(1) == '' ? 'MobileActiveMenu' : '' ?>"><span><i style="font-size: x-large;" class="fa-solid fa-house"></i></span></a></li>
-				<li class="MenuItems  px-0 mx-md-4 py-md-1 mt-3 " id="SearchIcon"><i style="font-size: x-large;" class="fa-solid fa-magnifying-glass"></i></li>
-				<li class="MenuItems  px-0 mx-md-4 py-md-1 mt-3 " data-toggle="modal" data-target="#MobileThemeMenu"><i style="font-size: x-large;" class="fa-solid fa-bars"></i></li>
+				<li class="MenuItems  px-0 mx-md-4 py-md-1 mt-3 "><a href="<?= base_url() ?>"
+																	 class="<?php echo $this->uri->segment(1) == '' ? 'MobileActiveMenu' : '' ?>"><span><i
+									style="font-size: x-large;" class="fa-solid fa-house"></i></span></a></li>
+				<li class="MenuItems  px-0 mx-md-4 py-md-1 mt-3 " id="SearchIcon"><i style="font-size: x-large;"
+																					 class="fa-solid fa-magnifying-glass"></i>
+				</li>
+				<li class="MenuItems  px-0 mx-md-4 py-md-1 mt-3 " data-toggle="modal" data-target="#MobileThemeMenu"><i
+							style="font-size: x-large;" class="fa-solid fa-bars"></i></li>
 			</ul>
 		</div>
 	</div>
 </div>
-<div class="container">
-	
-
+<div class="container d-block  d-md-none">
 	<!-- Modal -->
-	<div class="modal fade" id="MobileThemeMenu" tabindex="-1" role="dialog" aria-labelledby="MobileThemeMenuLabel" aria-hidden="true">
+	<div class="modal fade  " id="MobileThemeMenu" tabindex="-1" role="dialog" aria-labelledby="MobileThemeMenuLabel"
+		 aria-hidden="true">
 		<div class="ml-auto modal-dialog h-100 w-75 mt-0" role="document">
-			<div class="modal-content h-100">
-				<div class="modal-header">
-					<h5 class="modal-title" id="MobileThemeMenuLabel">Modal title</h5>
+			<div class="modal-content h-100" style="background: #e9e9e9e0;">
+				<div class="modal-header border-0 pb-0">
 					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 						<span aria-hidden="true">&times;</span>
 					</button>
 				</div>
-				<div class="modal-body">
+				<div class="align-items-center d-flex justify-content-around modal-body">
 					<ul class="list-unstyled">
-						<li><div>
+						<li class="h6">
+							<div>
 								<input type="checkbox" class="checkbox" id="checkbox">
-								<label for="checkbox" class="label">
+								<label for="checkbox" class="label mb-5 ml-3">
 									<i class="fas fa-moon"></i>
 									<i class='fas fa-sun'></i>
 									<div class='ball'>
 								</label>
-							</div></li>
-						<li><span><i class="fa-solid fa-face-grin-wink"></i></span>Explore</li>
-						<li><span><i class="fa-regular fa-bell"></i></span>Subscribe</li>
-						<li><span><i class="fa-solid fa-eye"></i></span>History</li>
-						<li><span><i class="fa-regular fa-circle-question"></i></span>Help & Feedback</li>
+							</div>
+						</li>
+						<li class="h6"><span><i class="fa-solid fa-face-grin-wink"></i></span>Explore</li>
+						<li class="h6"><span><i class="fa-regular fa-bell"></i></span>Subscribe</li>
+						<li class="h6"><span><i class="fa-solid fa-eye"></i></span>History</li>
+						<li class="h6"><span><i class="fa-regular fa-circle-question"></i></span>Help & Feedback</li>
 					</ul>
 				</div>
 
@@ -128,23 +154,22 @@
 	</div>
 </div>
 <script>
-$("#SearchIcon").click(function () {
-	$("#CarversalMobileMenu").hide('fast');
-$("#SearchHere").show('fast');
-})
-//
-$("#HideSearch").click(function () {
-$("#SearchHere").hide('fast');
-	$("#CarversalMobileMenu").show('fast');
-})
+	$("#SearchIcon").click(function () {
+		$("#CarversalMobileMenu").hide('fast');
+		$("#SearchHere").show('fast');
+	})
+	//
+	$("#HideSearch").click(function () {
+		$("#SearchHere").hide('fast');
+		$("#CarversalMobileMenu").show('fast');
+	})
 
 
+	const checkbox = document.getElementById('checkbox');
 
-const checkbox = document.getElementById('checkbox');
-
-checkbox.addEventListener('change', ()=>{
-	document.body.classList.toggle('dark');
-})
+	checkbox.addEventListener('change', () => {
+		document.body.classList.toggle('dark');
+	})
 </script>
 <script>var base_url = '<?=base_url()?>';</script>
 <script>var baseURL = '<?=base_url()?>';</script>
